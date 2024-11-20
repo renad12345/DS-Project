5ff7401d864bb2272eebc649bd92e50bd14650bc
raneem <raneem_alsaqat@hotmail.com>
2024-11-20 17:18:21 +0300

    comit

--- src/Document.java
+++ src/Document.java
@@ -1,3 +1,10 @@
 public class Document {
+    LinkedList<String>words = new LinkedList<>();
+ int id ;
+ 
+ public Document(int id, LinkedList<String>words,String content){
+  this.id =id;
+  this.words=words;
+  }
 
 }
